6704019a633128f03aa396e700916edf6a13522a
Luis Joseph Limgenco <marachljll@gmail.com>
2024-12-22 08:58:11 -0500

    updated table

--- initDb.js
+++ initDb.js
@@ -1,23 +1,19 @@
 import {ClickHouse} from 'clickhouse';
 
-// Initialize the ClickHouse client
 const clickhouse = new ClickHouse({
   url: 'http://127.0.0.1',
   port: 8123,
   debug: false,
   basicAuth: null,
   isUseGzip: false,
-  format: "json", // Default response format
+  format: "json", 
 });
 
-// Function to initialize the database and table
 async function initializeDatabase() {
   try {
-    // Create database
     await clickhouse.query("CREATE DATABASE IF NOT EXISTS user_activity").toPromise();
     console.log("Database 'user_activity' created or already exists.");
 
-    // Create table for storing user activity
     await clickhouse.query(`
       CREATE TABLE IF NOT EXISTS user_activity.events (
         userId UInt32,
@@ -36,5 +32,4 @@ async function initializeDatabase() {
   }
 }
 
-// Run the initialization
 initializeDatabase();
